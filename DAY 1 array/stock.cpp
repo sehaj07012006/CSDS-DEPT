3dcd3ab737a42a66b240c7abe77fae16472430d4
sehaj07012006 <sehajsingh191@gmail.com>
2026-05-31 22:59:23 +0530

    stock

---
+++ DAY 1 array/stock.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int maxProfit(vector<int> &price)
+{
+    int maxProfit = 0, bestBuy = price[0];
+    for (int i = 1; i < price.size(); i++)
+    {
+        if (price[i] > bestBuy)
+        {
+            maxProfit = max(maxProfit, price[i] - bestBuy);
+        }
+        bestBuy = min(bestBuy, price[i]);
+    }
+    return maxProfit;
+}
+int main(){
+    vector<int> Price = {4,5,3,9};
+    cout<<maxProfit(Price);
+}
