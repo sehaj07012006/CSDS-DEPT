c9e75e01cf5e7513fe333f139528c7d2584e70e3
sehaj07012006 <sehajsingh191@gmail.com>
2026-06-10 18:56:43 +0530

    repeatedSubstrPattern

---
+++ WEEK 2/DAY3/repeatedSubstr.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+bool repeatedSubstringPattern(string s)
+{
+    int n = s.length();
+    for (int l = n / 2; l >= 1; l--)
+    {
+        if (n % l == 0)
+        {
+            int times = n / l;
+            string pattern = s.substr(0, l);
+            string newStr = "";
+            while (times--)
+            {
+                newStr += pattern;
+            }
+            if (newStr == s)
+                return true;
+        }
+    }
+    return false;
+}
+int main(){
+    string s= "abababb";
+    cout<<repeatedSubstringPattern(s);
+}
