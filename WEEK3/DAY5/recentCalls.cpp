949d72d2f083578b1e954fa2e2a552f93319ccf6
sehaj07012006 <sehajsingh191@gmail.com>
2026-06-24 13:44:20 +0530

    recent Calls

---
+++ WEEK3/DAY5/recentCalls.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+private:
+    queue<int> q;
+
+public:
+    RecentCounter() {}
+
+    int ping(int t) {
+        q.push(t);
+
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
+
+int main() {
+    RecentCounter rc;
+
+    cout << rc.ping(1) << endl;
+    cout << rc.ping(100) << endl;
+    cout << rc.ping(3001) << endl;
+    cout << rc.ping(3002) << endl;
+
+    return 0;
+}
